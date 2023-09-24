d1c4d8a5b440b8de932656c06e3270bf0ea90e75
Jakosa1356 <145898253+Jakosa1356@users.noreply.github.com>
2023-09-24 15:45:10 +0700

    Commit from TeleportHQ

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home-container"></div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  head: {
+    title: 'Lovely Tart Lyrebird',
+    meta: [
+      {
+        property: 'og:title',
+        content: 'Lovely Tart Lyrebird',
+      },
+    ],
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
